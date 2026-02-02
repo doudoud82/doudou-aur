3fa5feff8e6f449ccebc41e1ad38ba6f6d11cc94
doudou <67341610+doudoud82@users.noreply.github.com>
2026-02-02 20:52:23 +0100

    typo fix

--- docker/build.sh
+++ docker/build.sh
@@ -9,8 +9,8 @@ if [[ ! -f "/repo/x86_64/doudou-aur.db.tar.gz" ]]; then
 fi
 
 sudo pacman -Sy --noconfirm
-mkdir -p "repo/aur-PKGBUILD"
-export AURDEST="repo/aur-PKGBUILD"
+mkdir -p "/repo/aur-PKGBUILD"
+export AURDEST="/repo/aur-PKGBUILD"
 
 packages_aur=$(grep -v '^\s*#' "$PKG_LIST" | grep -v '^\s*$' | tr '\n' ' ')
 for dir in "$PKG_DIR"/*/; do
@@ -19,5 +19,5 @@ for dir in "$PKG_DIR"/*/; do
     fi
 done
 aur sync --noview --sign --remove --upgrades --noconfirm  $packages_aur
-aur build --sign --clean --remove -a /tmp/pkgbuild_paths
+aur build --sign --clean --remove --new -a /tmp/pkgbuild_paths
 echo "Build complete. Repo is ready in x86_64."
