b3b88cded62e28ccba694cb438a3c3678bb02c1d
doudou <doudoud82@gmail.com>
2026-02-24 16:17:52 +0100

    temporary gawk fix

--- docker/build.sh
+++ docker/build.sh
@@ -11,6 +11,13 @@ fi
 sudo pacman -Sy --noconfirm
 mkdir -p "/repo/aur-PKGBUILD"
 export AURDEST="/repo/aur-PKGBUILD"
+export AUR_PAGER=cat
+# Temporary downgrade gawk to 5.3.2 because of error until it get fixed
+# https://github.com/aurutils/aurutils/issues/1245
+# gawk: /usr/lib/aurutils/aur-graph:72:     delete(arch)
+# gawk: /usr/lib/aurutils/aur-graph:72:           ^ syntax error
+
+sudo pacman -U https://archive.archlinux.org/packages/g/gawk/gawk-5.3.2-1-x86_64.pkg.tar.zst --noconfirm
 
 packages_aur=$(grep -v '^\s*#' "$PKG_LIST" | grep -v '^\s*$' | tr '\n' ' ')
 for dir in "$PKG_DIR"/*/; do
